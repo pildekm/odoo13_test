7253fd78b370fba7148b2288d6f9978aad3d7d40
matija <matija.pildek@gmail.com>
2020-05-07 15:02:52 +0200

    dodan cookie notice

--- website_cookie_notice/__manifest__.py
+++ website_cookie_notice/__manifest__.py
@@ -2,22 +2,17 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Cookie notice',
-    'summary': 'Show cookie notice according to cookie law',
-    'version': '12.0.1.0.0',
-    'category': 'Website',
-    'author': "Agile Business Group, "
-              "Tecnativa, "
-              "Nicolas JEUDY, "
-              "Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/website',
-    'license': 'AGPL-3',
-    'depends': [
-                # 'website',
-                # 'web_editor',
-                # 'website_legal_page',
-                ],
-    'data': [
-        'templates/website.xml',
-    ],
+    "name": "Cookie notice",
+    "summary": "Show cookie notice according to cookie law",
+    "version": "13.0.1.0.0",
+    "category": "Website",
+    "author": "Agile Business Group, "
+    "Tecnativa, "
+    "Nicolas JEUDY, "
+    "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/website",
+    "license": "AGPL-3",
+    "depends": ["website_legal_page"],
+    "data": ["views/assets.xml", "views/website.xml"],
+    "post_init_hook": "post_init_hook",
 }
--- website_cookie_notice/controllers/main.py
+++ website_cookie_notice/controllers/main.py
@@ -3,27 +3,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import http
-from odoo.http import request
-from odoo.addons.website.controllers.main import Website
 
 
 class CookieNotice(http.Controller):
-
-    @http.route("/website_cookie_notice/ok", auth="public", website=True, type='json',methods=['POST'])
+    @http.route(
+        "/website_cookie_notice/ok",
+        auth="public",
+        website=True,
+        type="json",
+        methods=["POST"],
+    )
     def accept_cookies(self):
         """Stop spamming with cookie banner."""
         http.request.session["accepted_cookies"] = True
-        http.request.env['ir.ui.view'].search([
-            ('type', '=', 'qweb')
-        ]).clear_caches()
-        return {'result': 'ok'}
-
-class WebsiteCookie(Website):
-
-    @http.route('/', type='http', auth="public", website=True)
-    def index(self, **kw):
-        res = super(WebsiteCookie, self).index(**kw)
-        print('Cookie pičko')
-        values = {'button_name': 'Yes'}
-        return request.render('website_cookie_notice.cookiebanner', values)
-
+        http.request.env["ir.ui.view"].search([("type", "=", "qweb")]).clear_caches()
+        return {"result": "ok"}
